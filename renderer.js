cc24fbae879a4f6de67c1069fa51227af523ebe7
Igor Trishin <yaschur@gmail.com>
2017-02-16 18:23:39 +0300

    turn it right way

--- renderer.js
+++ renderer.js
@@ -1,4 +1,13 @@
-const Sequelize = require('sequelize');
-require('sqlite3');
-var sequelize = new Sequelize('test', null, null, {dialect:'sqlite', storage: 'd:\\temp\\t.db'});
+var pouchDb = new PouchDB('test');
 
+var tObj = {
+	_id: '123',
+	name: 'alpha'
+};
+
+pouchDb.put(tObj);
+pouchDb.find({
+	selector: {_id: '123'}
+});
+
+pouchDb.info().then((info) => console.log(info));
