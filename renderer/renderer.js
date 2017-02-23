3ad9636144514b6cc43120a78021d8eb4ae9089c
Igor Trishin <yaschur@gmail.com>
2017-02-23 19:18:41 +0300

    restart

--- renderer/renderer.js
+++
@@ -1,9 +0,0 @@
-var pouchDb = new PouchDB('test');
-var company = new Company('name', 'rus', '123', 'http://');
-
-pouchDb.put(company);
-pouchDb.find({
-	selector: {_id: '123'}
-});
-
-pouchDb.info().then((info) => console.log(info));
