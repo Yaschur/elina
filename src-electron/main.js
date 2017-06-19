1b39db65054aeafdcb1b01454b23ded7cfe03c72
Igor Trishin <yaschur@gmail.com>
2017-06-19 15:58:06 +0300

    start new electron shell

---
+++ src-electron/main.js
@@ -0,0 +1,66 @@
+const electron = require('electron');
+const { app, BrowserWindow, globalShortcut } = electron;
+const path = require('path');
+const url = require('url');
+
+let mainWindow = null;
+
+const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+	// Someone tried to run a second instance, we should focus our window.
+	if (mainWindow) {
+		if (mainWindow.isMinimized())
+			mainWindow.restore();
+		mainWindow.focus();
+	}
+})
+
+if (shouldQuit) {
+	app.quit();
+}
+
+function createWindow() {
+	const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+	// Create the browser window.
+	mainWindow = new BrowserWindow({ width: width, height: height });
+	mainWindow.setMenu(null);
+
+	// and load the index.html of the app.
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'clt/index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	// Open the DevTools.
+	globalShortcut.register('Ctrl+Shift+D', () => mainWindow.toggleDevTools());
+
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', function () {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
