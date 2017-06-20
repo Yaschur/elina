06d783135bca47be6d442785d8ebd26cf69e0158
Igor Trishin <yaschur@gmail.com>
2017-06-20 18:15:16 +0300

    implement config service

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+
+import { ElectronService } from 'ngx-electron';
 
 interface Config {
-	database: {
-		nameOrUrl: string;
-		backupAllowed: boolean;
-		user?: string;
-		password?: string;
+	readonly database: {
+		readonly nameOrUrl: string;
+		readonly backupAllowed: boolean;
+		readonly user?: string;
+		readonly password?: string;
 	}
 }
 
 @Injectable()
 export class ConfigService {
 
-	constructor() { }
+	private _current: BehaviorSubject<Config>;
+
+	constructor(private _electronService: ElectronService) {
+		this.init();
+		this._electronService.ipcRenderer.send('load-config');
+	}
+
+	get currentConfig(): Observable<Config> {
+		return this._current.asObservable();
+	}
+
+	private init() {
+		if (this._electronService.isElectronApp) {
+			this._electronService.ipcRenderer.on('config-loaded', (event, arg) => {
+				console.log('receive config loaded...');
+				this._current.next(JSON.parse(arg));
+			});
+		}
+	}
 }
