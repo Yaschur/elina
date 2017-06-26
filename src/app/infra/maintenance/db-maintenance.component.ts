368f999099cacb6f6164fc9d6c9dd31998b67553
Igor Trishin <yaschur@gmail.com>
2017-06-26 12:28:43 +0300

    add ui for mnts (failed)

--- src/app/infra/maintenance/db-maintenance.component.ts
+++ src/app/infra/maintenance/db-maintenance.component.ts
@@ -2,14 +2,22 @@ import { Component, OnInit } from '@angular/core';
 // import { Component, OnInit, NgZone } from '@angular/core';
 import { ElectronService } from 'ngx-electron';
 
+import { DbMaintService } from '../store/db-maint.service';
+
 @Component({
 	selector: 'app-db-maintenance',
 	templateUrl: './db-maintenance.component.html'
 })
 
 export class DbMaintenanceComponent implements OnInit {
-	// info = '';
-	constructor(private _electronService: ElectronService) { }
+
+	replicationStatus = '';
+	transferStatus = '';
+
+	constructor(
+		private _electronService: ElectronService,
+		private _dbMaintService: DbMaintService
+	) { }
 
 	ngOnInit() {
 		if (this._electronService.isElectronApp) {
@@ -20,5 +28,26 @@ export class DbMaintenanceComponent implements OnInit {
 			// });
 		}
 		// this._electronService.ipcRenderer.send('load-config');
+	}
+
+	backup() {
+		this.replicationStatus = 'Database backup is started...';
+		this._dbMaintService.doBackup()
+			.then(() => this.replicationStatus = 'Database backup is completed.')
+			.catch(e => this.replicationStatus = 'Database backup error:' + e);
+	}
+
+	restore() {
+		this.replicationStatus = 'Database restoring is started...';
+		this._dbMaintService.doRestore()
+			.then(() => this.replicationStatus = 'Database restoring is completed.')
+			.catch(e => this.replicationStatus = 'Database restoring error:' + e);
+	}
+
+	clear() {
+		this.replicationStatus = 'Database clearing is started...';
+		this._dbMaintService.doClear()
+			.then(() => this.replicationStatus = 'Database clearing is completed.')
+			.catch(e => this.replicationStatus = 'Database clearing error:' + e);
 	}
 }
