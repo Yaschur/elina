de2fc16085b68fd23abe79bcf4486f7f9a520d7e
Igor Trishin <yaschur@gmail.com>
2017-07-20 18:14:20 +0300

    add get bu refs to participants repo

--- src/app/participants/repositories/participant.repository.ts
+++ src/app/participants/repositories/participant.repository.ts
@@ -23,6 +23,27 @@ export class ParticipantRepository {
 		return new Participant(dbItem);
 	}
 
+	public async getByAllRefIds(eventId: string, companyId: string, contactId: string): Promise<Participant> {
+		const res = (await this._storeService.find(
+			ParticipantRepository.entityType,
+			{
+				$and: [
+					{ company: { $eq: companyId } },
+					{ contact: { $eq: contactId } },
+					{ event: { $eq: eventId } }
+				]
+			},
+			[]
+		)).map(dbItem => new Participant(dbItem));
+		if (res.length > 1) {
+			console.log(
+				'more than one participants are found, event: ' + eventId + ', company: ' + companyId + ', contact: ' + contactId
+			);
+		}
+
+		return res.length > 0 ? res[0] : null;
+	}
+
 	public async FindByEvent(eventId: string): Promise<Participant[]> {
 		return (await this._storeService.find(
 			ParticipantRepository.entityType,
