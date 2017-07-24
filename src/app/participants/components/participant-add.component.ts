d842b818692e67d6fd84a375a4a24f94e42b0f88
Igor Trishin <yaschur@gmail.com>
2017-07-24 18:31:40 +0300

    start add participant component

---
+++ src/app/participants/components/participant-add.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-participant-add',
+	templateUrl: 'participant-add.component.html'
+})
+
+export class ParticipantAddComponent implements OnInit {
+	constructor() { }
+
+	ngOnInit() { }
+}
