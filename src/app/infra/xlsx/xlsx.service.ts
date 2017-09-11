6801e4ab53c7e6d3e5c5da4922010dd387b13f53
Igor Trishin <yaschur@gmail.com>
2017-09-11 10:58:32 +0300

    overcome it

--- src/app/infra/xlsx/xlsx.service.ts
+++ src/app/infra/xlsx/xlsx.service.ts
@@ -10,46 +10,18 @@ export class XlsxService {
 
 	async exportToXlsx<T>(data: T[], filename: string, sheetName: string): Promise<void> {
 		const wbook = await XlsxPopulate.fromBlankAsync();
-		console.log(XlsxPopulate.MIME_TYPE);
 		const wsheet = wbook.sheet(0);
-		console.log(wsheet.name());
-		wsheet.name('Renamed sheet');
-		wsheet.cell(0, 0).value('Here we goooo ooo ooo');
-		wsheet.cell(0, 1).value('Go we hereeee eee eee');
-		wsheet.cell(0, 0).style({ shrinkToFit: true });
-		wsheet.cell(0, 1).style({ wrapText: true });
-		// const strOut = await wbook.outputAsync('string');
-		// console.log(strOut);
-		// const blobOut = await wbook.outputAsync();
-		// console.log(blobOut);
-		// saveAs(blobOut, filename);
-		// const wbout = await wbook.outputAsync('binarystring');
-		const buf = await wbook.outputAsync('binarystring');
-		console.log(buf);
-		// new ArrayBuffer(wbout.length);
-		// const view = new Uint8Array(buf);
-		// for (let i = 0; i !== wbout.length; ++i) {
-		// 	// tslint:disable-next-line:no-bitwise
-		// 	view[i] = wbout.charCodeAt(i) & 0xFF;
-		// }
-		// const wbout = await wbook.outputAsync('blob');
-		// const url = window.URL.createObjectURL(wbout);
-		// console.log(url);
-		// window.open(url);
-		saveAs(new Blob([buf]), filename);
+		wsheet.name(sheetName);
+		const keys = Object.keys(data[0]);
+		keys.forEach((key, ind) => wsheet.cell(1, ind + 1).value(key));
+		wsheet.row(1).style({ bold: true, horizontalAlignment: 'center' });
+		data.forEach((item, r) =>
+			keys.forEach((key, ind) =>
+				wsheet.cell(r + 2, ind + 1).value(item[key].toString())
+			)
+		);
 
-		// const wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'binary' };
-		// const worksheet = XLSX.utils.json_to_sheet(data);
-		// worksheet['A1'].v = worksheet['A1'].s;
-		// const workbook = XLSX.utils.book_new();
-		// XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
-		// const wbout = <string>XLSX.write(workbook, wopts);
-		// const buf = new ArrayBuffer(wbout.length);
-		// const view = new Uint8Array(buf);
-		// for (let i = 0; i !== wbout.length; ++i) {
-		// 	// tslint:disable-next-line:no-bitwise
-		// 	view[i] = wbout.charCodeAt(i) & 0xFF;
-		// }
-		// saveAs(new Blob([buf]), filename);
+		const blobOut = await wbook.outputAsync();
+		saveAs(blobOut, filename);
 	}
 }
