89dd7a516eac21483872de475ff57f3075a45fcb
Igor Trishin <yaschur@gmail.com>
2017-10-25 18:42:36 +0300

    do reworking xupdate

--- src/app/xupdate/components/xupdate.component.rw.ts
+++ src/app/xupdate/components/xupdate.component.rw.ts
@@ -8,3 +8,25 @@ export class XupdateComponent implements OnInit {
 	ngOnInit() {
 	}
 }
+
+class ImportedItem {
+	companyKey: string;
+	contactKey: string;
+	companyDatas: CompanyData[];
+	contactDatas: ContactData[];
+}
+
+class CompanyData {
+	name: string;
+	countryName: string;
+	country: string;
+	website: string;
+}
+
+class ContactData {
+	firstName: string;
+	lastName: string;
+	jobTitle: string;
+	phone: string;
+	email: string;
+}
