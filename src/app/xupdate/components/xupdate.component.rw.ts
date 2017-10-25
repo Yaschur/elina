68ed7405a33474baa4c402feff34c9ea54d86d0e
Igor Trishin <yaschur@gmail.com>
2017-10-25 17:21:57 +0300

    start reworking xupdate

---
+++ src/app/xupdate/components/xupdate.component.rw.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-xupdate',
+	templateUrl: './xupdate.component.html'
+})
+export class XupdateComponent implements OnInit {
+	ngOnInit() {
+	}
+}
