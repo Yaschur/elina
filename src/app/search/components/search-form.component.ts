7c17d2b41985869b722e1f20fc3a8abb34d69986
Igor Trishin <yaschur@gmail.com>
2017-11-28 10:39:32 +0300

    fix ui

--- src/app/search/components/search-form.component.ts
+++ src/app/search/components/search-form.component.ts
@@ -16,7 +16,8 @@ import { SearchCriteriaManager, SearchCriteria } from './criteria.model';
 
 @Component({
 	selector: 'app-search-form',
-	templateUrl: './search-form.component.html'
+	templateUrl: './search-form.component.html',
+	styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
 
