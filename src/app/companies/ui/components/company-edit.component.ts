3387d320faf7086fec21a74edc0912d1aada1b26
Igor Trishin <yaschur@gmail.com>
2018-02-20 17:50:22 +0300

    add deleting company if no participation

--- src/app/companies/ui/components/company-edit.component.ts
+++ src/app/companies/ui/components/company-edit.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Location } from '@angular/common';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Observable } from 'rxjs/Observable';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap';
 
 import { DirectoryService, Country, Activity } from '../../../directories';
 import { Company, CompanyRepository } from '../../core';
+import { ParticipantRepository } from '../../../participants';
 
 @Component({
 	selector: 'app-company-edit',
@@ -14,32 +16,42 @@ import { Company, CompanyRepository } from '../../core';
 })
 export class CompanyEditComponent implements OnInit {
 	company: Company;
+	canBeDeleted: boolean;
 
 	countries: Observable<Country[]>;
 	activities: Observable<Activity[]>;
 
 	companyForm: FormGroup;
 
+	modalRef: BsModalRef;
+
 	constructor(
 		private _companyRepo: CompanyRepository,
+		private _partyRepo: ParticipantRepository,
 		private _route: ActivatedRoute,
 		private _router: Router,
 		private _location: Location,
 		private _fb: FormBuilder,
-		private _dirSrv: DirectoryService
+		private _dirSrv: DirectoryService,
+		private _modalService: BsModalService
 	) {
 		this.countries = this._dirSrv.getDir('country').data;
 		this.activities = this._dirSrv.getDir('activity').data;
+		this.canBeDeleted = false;
 		this.createForm();
 	}
 
 	ngOnInit() {
-		this._route.params
-			.switchMap(params => this._companyRepo.getById(params['id']))
+		const cobs = this._route.params
+			.switchMap(params => this._companyRepo.getById(params['id']));
+		cobs
 			.subscribe(item => {
 				this.company = item;
 				this.initForm();
 			});
+		cobs
+			.switchMap(item => item ? this._partyRepo.FindByCompany(item._id) : Observable.of([]))
+			.subscribe(pts => this.canBeDeleted = pts.length === 0);
 	}
 
 	onSubmit() {
@@ -64,6 +76,21 @@ export class CompanyEditComponent implements OnInit {
 
 	onCancel() {
 		this._location.back();
+	}
+
+	deleteConfirmation(template: TemplateRef<any>) {
+		this.modalRef = this._modalService.show(template, { keyboard: false, backdrop: false, ignoreBackdropClick: true });
+	}
+	deleteCancelled() {
+		this.modalRef.hide();
+		this.modalRef = undefined;
+	}
+	deleteConfirmed() {
+		this.modalRef.hide();
+		this.modalRef = undefined;
+		this._companyRepo.remove(this.company)
+			.then(() => this._router.navigate(['company']))
+			.catch(e => console.log(e));
 	}
 
 	private createForm() {
